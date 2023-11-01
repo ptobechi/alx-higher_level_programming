9763d870adb06e3114a55ee92650aea682696555
Tobechukwu <ptobechii@gmail.com>
2023-11-01 18:09:22 -0500

    print last digit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
  #!/usr/bin/python3
+
  def uppercase(s):
     for char in s:
         if 'a' <= char <= 'z':
-            uppercase_char = chr(ord(char) - 32)
+            upper = chr(ord(char) - 32)
         else:
-            uppercase_char = char
-        print(uppercase_char, end='')
+            upper = char
+        print(upper, end='')
