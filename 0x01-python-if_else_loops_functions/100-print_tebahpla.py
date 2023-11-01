0e51b8781efe3051d43698b9648d2d92b89b2bd3
Tobechukwu <ptobechii@gmail.com>
2023-11-01 18:19:24 -0500

    print last digit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for char in range(90, 64, -1):
-    print(f"{chr(char)}", end="")
-    char += 32 if char % 2 == 0 else 0
+    if char % 2 == 0:
+        print('{}'.format(chr(char)), end='')
+    else:
+        print('{}'.format(chr(char).lower()), end='')
