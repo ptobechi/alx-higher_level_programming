fad3fcc2098dee3985bc35749ac106949dcc5cb5
Tobechukwu <ptobechii@gmail.com>
2023-11-01 17:53:08 -0500

    print last digit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 for char in range(97, 123):
-    if chr(char) not in 'qe':
-        print(chr(char), end='')
-
+    if chr(char) not in ['e', 'q']:
+        print('{}'.format(chr(char)), end='')
