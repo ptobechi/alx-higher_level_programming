ed285dc04b23005d459a503b847ab1cc1bfa6d5b
Tobechukwu <ptobechii@gmail.com>
2023-11-01 16:41:55 -0500

    print last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
 
-# Get the last digit of the number
 last_digit = abs(number) % 10
-
-# Determine the conditions and build the output string
-output = "Last digit of {} is {} and is".format(number, last_digit)
+comparison = ""
 
 if last_digit > 5:
-    output += " greater than 5"
+    comparison = "greater than 5"
 elif last_digit == 0:
-    output += " 0"
+    comparison = "0"
 else:
-    output += " less than 6 and not 0"
+    comparison = "less than 6 and not 0"
 
-print(output)
+print(f"Last digit of {number} is {last_digit} and is {comparison}\n")
