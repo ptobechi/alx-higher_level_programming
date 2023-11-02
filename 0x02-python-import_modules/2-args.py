6ce59689b47bb27bde6d3ebfc452c2952cc77668
Tobechukwu <ptobechii@gmail.com>
2023-11-02 15:52:58 -0500

    a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     argv = sys.argv
     num = len(argv)
     if num == 1:
-        print(f"{n-1:d} arguments.")
+        print(f"{num-1:d} arguments.")
     else:
-        print(f"{n-1:d} argument:" if num == 2 else f"{n-1:d} arguments:")
-        for i inum range(1, n):
+        print(f"{num-1:d} argument:" if num == 2 else f"{num-1:d} arguments:")
+        for i in range(1, num):
             print(f"{i:d}: {argv[i]:s}")
