526199e55f37dd877095544af1e802462a4b1e06
Ghost <ptobechii@gmail.com>
2023-11-22 15:05:08 +0100

    a function that raises a name exception with a message

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,12 +5,11 @@ def safe_print_division(a, b):
     try:
         result = a / b
         return a / b
-    except:
+    except ZeroDivisionError:
         result = None
         return None
     finally:
         print("Inside result: {}".format(result))
-
 # a = 12
 # b = 2
 # result = safe_print_division(a, b)
