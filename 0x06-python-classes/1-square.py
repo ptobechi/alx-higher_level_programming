d4f21b6a2766f231d2c654fd8631a74f50f4b8bf
Ghost <ptobechii@gmail.com>
2023-11-22 20:35:14 +0100

    a class Square that defines a suqare

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""A square class"""
+
+
+class Square:
+    """A square class that defines a square"""
+
+    def __init__(self, size=0):
+        """size of the square"""
+        self.__size = size
+
+# my_square = Square(3)
+# print(type(my_square))
+# print(my_square.__dict__)
+
+# try:
+#     print(my_square.size)
+# except Exception as e:
+#     print(e)
+
+# try:
+#     print(my_square.__size)
+# except Exception as e:
+#     print(e)
