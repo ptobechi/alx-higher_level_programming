b52cd790b158b01c112e1a3bbe29f958a95e6188
Ghost <ptobechii@gmail.com>
2023-11-22 15:06:33 +0100

    a function that raises a name exception with a message

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,7 +17,7 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             new_list.append(result)
     return new_list
-  
+
 # my_l_1 = [10, 8, 4]
 # my_l_2 = [2, 4, 4]
 # result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
