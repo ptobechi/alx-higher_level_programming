1ac09268735002a0b69a916727c95dfb3c96611c
Ghost <ptobechii@gmail.com>
2023-11-22 22:47:26 +0100

    pycode styles indentation fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_function(fct, *args):
     try:
         result = fct(*args)
         return result
-
     except Exception as e:
         sys.stderr.write(f"Exception: {e}\n")
         return None
