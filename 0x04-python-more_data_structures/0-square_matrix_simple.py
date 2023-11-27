240d32947eda3b200953ad687cc19ea8e16ec651
Tobechukwu <ptobechii@gmail.com>
2023-11-27 08:36:14 -0600

    reade me file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,7 +6,7 @@ def square_matrix_simple(matrix=[]):
     # Iterate through the rows and columns of the input matrix
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            # Compute the square of each element and store it in the result matrix
+            # Compute the square of each el & store it in the result matrix
             result_matrix[i][j] = matrix[i][j] ** 2
 
     return result_matrix
