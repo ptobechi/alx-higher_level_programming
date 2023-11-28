7e9a960fdaec310722df674e13bb41ec58c462a8
Tobechukwu <ptobechii@gmail.com>
2023-11-27 18:24:04 -0600

    empty rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+A pyhton script that defines a Rectangle class
+
+The Rectangle class is a rectangle shape and can be used as a
+blueprint for creating rectangle objects
+"""
+
+class Rectangle:
+    """
+    A Rectnagle class.
+
+    Attributes:
+        No attributes defined
+    Methods:
+        No method defined
+    """
+    pass
+
+#my_rectangle = Rectangle()
+#print(type(my_rectangle))
+#print(my_rectangle.__dict__)
+
