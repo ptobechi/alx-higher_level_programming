0a5d6e3f864be7048ba4b81aef4d586259f3205e
Tobechukwu <ptobechii@gmail.com>
2023-11-29 18:40:06 -0600

    inittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 
-"""
-a python script with unittest
-"""
-
-
 import unittest
-from max_integer import max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """
