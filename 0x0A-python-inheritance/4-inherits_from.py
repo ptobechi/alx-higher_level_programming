9a0d72474fcd5f3fc0461be8255abfff6b3686d0
ptobechi <ptobechii@gmail.com>
2023-12-04 16:22:50 -0600

    complete project

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -19,12 +19,12 @@ def inherits_from(obj, a_class):
     :Return:
         True if obj is an instance of a subclass of a_class, False otherwise.
     """
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
 
 # a = True
 # if inherits_from(a, int):
-#  print("{} inherited from class {}".format(a, int.__name__))
+#   print("{} inherited from class {}".format(a, int.__name__))
 # if inherits_from(a, bool):
-#  print("{} inherited from class {}".format(a, bool.__name__))
+#   print("{} inherited from class {}".format(a, bool.__name__))
 # if inherits_from(a, object):
 #  print("{} inherited from class {}".format(a, object.__name__))
