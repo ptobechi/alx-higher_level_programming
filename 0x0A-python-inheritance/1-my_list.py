4a842cc653859e20d502912886b4626bf898e647
ptobechi <ptobechii@gmail.com>
2023-12-04 15:30:33 -0600

    a class MyList that inherits from list:

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+A module that demostrates a Class that inherites from the builtin list class
+"""
+
+
+class MyList(list):
+    """
+    A custom list class that inherits from the built-in list class.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending order.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
+# my_list = MyList()
+# my_list.append(1)
+# my_list.append(4)
+# my_list.append(2)
+# my_list.append(3)
+# my_list.append(5)
+# print(my_list)
+# my_list.print_sorted()
+# print(my_list)
+
