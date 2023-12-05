f39dfa4e05d1ca4f67b8029bd01105161095feb1
ptobechi <ptobechii@gmail.com>
2023-12-05 14:02:34 -0600

    indentation fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,7 @@
 a module that add all the elements of a python script
 """
 import sys
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
