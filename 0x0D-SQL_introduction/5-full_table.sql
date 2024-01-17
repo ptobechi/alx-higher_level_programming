99e9ae658e64b615f5fe1ea1746e7a4df64f8b71
ptobechi <ptobechii@gmail.com>
2024-01-17 10:21:39 -0600

    corrections

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
 -- Script to print the full description of the table first_table
--- Query to retrieve the full description of the table first_table */
-SHOW COLUMNS FROM first_table;
+-- Query to retrieve the full description of the table first_table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
