8d86d0bf255443084b7bc2a5676b5f56961fb378
ptobechi <ptobechii@gmail.com>
2024-01-17 10:06:08 -0600

    corrections

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,3 @@
 -- Script to list all tables of a specified database in MySQL server
--- Checking if the argument (database name) is provided */
-USE hbtn_0c_0;
-
 -- Query to list all tables in the specified database
 SHOW TABLES;
