254e85428fc3155021d3b7c4cdfdcad22235b756
ptobechi <ptobechii@gmail.com>
2024-03-01 10:20:30 -0600

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request, and displays the body of the response for a 200 status code
-
-# Usage: ./1-body.sh <URL>
-
-# Make the GET request with curl and display the body for a 200 status code
-curl -sL -w "%{http_code}" "$1" -o /dev/null | \
-  awk '{if($1 == 200) system("curl -sL "$1)}'
-
+curl -sL -w "%{http_code}" "$1" -o /dev/null | awk '{if($1 == 200) system("curl -sL "$1)}'
